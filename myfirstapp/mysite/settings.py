79dab1b4ff2e5e80e0b4a00191259f4942e96bbd
yatichapat <yatichapat.d@ku.th>
2025-08-28 02:07:02 +0000

    add templates

--- myfirstapp/mysite/settings.py
+++ myfirstapp/mysite/settings.py
@@ -105,11 +105,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bangkok'
 
 USE_I18N = True
 
 USE_TZ = True
+
+USE_L10N = True
 
 
 # Static files (CSS, JavaScript, Images)
